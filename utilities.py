ddb3693acc8607b4bf420ba862737857bbc7610a
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2022-09-02 19:22:59 -0700

    feat(demo): empty code for the demo

--- utilities.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-def save_image(image_url):
-    img_data = requests.get(image_url).content
-    with open('resources/image.jpg', 'wb') as handler:
-        handler.write(img_data)
