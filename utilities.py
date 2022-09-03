bb5ba279b0f24b13bfe5348d0a1d6a46c7098e90
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2022-09-03 01:10:20 -0400

    fix(image): move image location

--- utilities.py
+++ utilities.py
@@ -3,5 +3,5 @@ import requests
 
 def save_image(img_url):
     img_data = requests.get(img_url).content
-    with open('image.jpg', 'wb') as handler:
+    with open('resources/image.jpg', 'wb') as handler:
         handler.write(img_data)
